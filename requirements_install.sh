329b901334a5be6df55996e57f3ca4b4171d1bd3
szfire <49318148@qq.com>
2025-09-20 15:42:56 +0800

    change setup sh

--- requirements_install.sh
+++ requirements_install.sh
@@ -7,3 +7,10 @@ pip install numpy==1.23.5
 pip install Cmake
 
 pip install -r requirements.txt
+
+cd submodules/diff-gaussian-rasterization
+pip install -e .
+
+cd ../simple-knn
+pip install -e .
+cd ../..
