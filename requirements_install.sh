6e0b1f16c8b9ea0d9b6c794a4fb44ef92bf0315a
szfire <49318148@qq.com>
2025-09-20 15:41:15 +0800

    add

---
+++ requirements_install.sh
@@ -0,0 +1,9 @@
+conda create -n scgs python=3.10
+conda activate scgs
+
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+pip install setuptools==69.5.1
+pip install numpy==1.23.5
+pip install Cmake
+
+pip install -r requirements.txt
