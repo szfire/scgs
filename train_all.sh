68c0b6af8c518914fec1c137399a348ebe5853fb
szfire <49318148@qq.com>
2025-09-28 11:07:16 +0800

    test

--- train_all.sh
+++ train_all.sh
@@ -5,11 +5,12 @@ GPU_ID=$1  # 修复等号两边空格问题
 run_pipeline() {
 
     # 运行训练脚本
-    CUDA_VISIBLE_DEVICES="$GPU_ID" python train_gui_51.py \
-    --source_path data_spacetime/"$data_name_path"/colmap_0 \
-    --model_path logs/technicolorvalid/"$data_name"_8 \
+    # Train with terminal only (for the resolution of 400*400 with best PSNR)
+    CUDA_VISIBLE_DEVICES="$GPU_ID" python train_gui.py \
+    --source_path /data/szz/SCGS/scgs/data/"$data_name" \
+    --model_path outputs/"$data_name" \
     --deform_type node \
-    --node_num 1024 \
+    --node_num 512 \
     --hyper_dim 8 \
     --is_blender \
     --eval \
@@ -17,27 +18,27 @@ run_pipeline() {
     --local_frame \
     --resolution 2 \
     --W 800 \
-    --H 800 \
-    --config arguments/"$data_name_path".py
+    --H 800
+
     
 
 
 
     # 运行渲染脚本
-    CUDA_VISIBLE_DEVICES="$GPU_ID" python render5_b.py \
-    --source_path data_spacetime/"$data_name_path"/colmap_0 \
-    --model_path logs/technicolorvalid/"$data_name"_8 \
-    --deform_type node \
-    --node_num 1024 \
-    --hyper_dim 8 \
-    --is_blender \
-    --eval \
-    --gt_alpha_mask_as_scene_mask \
-    --local_frame \
-    --resolution 2 \
-    --W 800 \
-    --H 800 \
-    --dataset_type $valloader
+    # CUDA_VISIBLE_DEVICES="$GPU_ID" python render5_b.py \
+    # --source_path data_spacetime/"$data_name_path"/colmap_0 \
+    # --model_path logs/technicolorvalid/"$data_name"_8 \
+    # --deform_type node \
+    # --node_num 1024 \
+    # --hyper_dim 8 \
+    # --is_blender \
+    # --eval \
+    # --gt_alpha_mask_as_scene_mask \
+    # --local_frame \
+    # --resolution 2 \
+    # --W 800 \
+    # --H 800 \
+    # --dataset_type $valloader
 }
 
 # 定义NeRF-DS数据集数组
@@ -46,12 +47,14 @@ run_pipeline() {
 #Neural3D_DA=("flame_salmon_1" "coffee_martini" "cut_roasted_beef" "sear_steak" "flame_steak" "cook_spinach")
 
 #Neural3D_DA=("Painter" "Birthday" "Train" "Fatma" "Hands") # "Painter" "Birthday" "Train" "Fatma" "Hands")  "Fabien"  "Theater" #( "Fatma" "Hands") #("coffee_martini" "flame_steak") # "Birthday" "Fabien"   "Fabien" "Theater" "Train"  "Theater"
-Neural3D_DA=( "Birthday")
-valloader="technicolorvalid"
+#Neural3D_DA=( "Birthday")
+
+D-NeRF=("bouncingballs" "hellwarrior" "hook" "jumpingjacks" "lego" "mutant" "standup" "trex")
+# valloader="technicolorvalid"
 # 循环遍历数组并调用run_pipeline函数
-for data_name in "${Neural3D_DA[@]}"; 
+for data_name in "${NeRF[@]}"; 
 do
-    echo "Dataset: Technicolor/${data_name}"
-    data_name_path="Technicolor/${data_name}"
-    run_pipeline "$GPU_ID" "$data_name_path" "$data_name" "$valloader"
+    echo "Dataset: D-NeRF/${data_name}"
+    # data_name_path="Technicolor/${data_name}"
+    run_pipeline "$GPU_ID" "$data_name"
 done
