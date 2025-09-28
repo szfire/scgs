9a8d3bba4c8bc11524c2c51deac9778c9ba6155f
szfire <49318148@qq.com>
2025-09-28 11:34:38 +0800

    add install info

--- requirements_install.sh
+++ requirements_install.sh
@@ -1,10 +1,12 @@
-conda create -n scgs python=3.10
-conda activate scgs
-
-pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+#!/bin/bash
+#根据
+#pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
 pip install Cmake
 
 pip install -r requirements.txt
+pip install imageio-ffmpeg==0.6.0
+pip install dearpygui==2.0.0
+pip install 
 
 cd submodules/diff-gaussian-rasterization
 pip install -e .
@@ -13,5 +15,3 @@ cd ../simple-knn
 pip install -e .
 cd ../..
 
-CUDA_VISIBLE_DEVICES=0 python render.py --source_path D:/work/project/SCGS/scgs/data/jumpingjacks --model_path outputs/jumpingjacks --deform_type node --node_num 512 --hyper_dim 8 --is_blender --eval --gt_alpha_mask_as_scene_mask --local_frame --resolution 2 --W 800 --H 800
-CUDA_VISIBLE_DEVICES=0 python render.py --source_path /data/szz/scgs/data/jumpingjacks --model_path outputs/jumpingjacks --deform_type node --node_num 512 --hyper_dim 8 --is_blender --eval --gt_alpha_mask_as_scene_mask --local_frame --resolution 2 --W 800 --H 800
